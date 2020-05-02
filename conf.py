d09547184c4e74aece42c414bdeabcebe6851094
Huzaifa Irfan <huzaifairfan2001@gmail.com>
2020-05-02 15:29:22 +0500

    1

--- conf.py
+++ conf.py
@@ -12,11 +12,11 @@
 #towerheight: The Height of Tower in the Game
 
 difficulties=[
-    {"name":"Easy","high":15,"low":1,"getagain":2,"towerheight":5}
+    {"name":"Easy","high":20,"low":1,"getagain":2,"towerheight":5}
     ,{"name":"Normal","high":40,"low":1,"getagain":5,"towerheight":10}
     ,{"name":"Hard","high":50,"low":1,"getagain":10,"towerheight":20}
     ,{"name":"Very Hard","high":100,"low":1,"getagain":15,"towerheight":30}
-    ,{"name":"Heroic","high":200,"low":0,"getagain":20,"towerheight":40}
+    ,{"name":"Heroic","high":150,"low":1,"getagain":20,"towerheight":40}
     ]
 
 
